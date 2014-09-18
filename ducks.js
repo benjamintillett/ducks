06d5a96fa658fe79aaf706e612b3f2723d736884
benjamintillett <benjamin.tillett@gmail.com>
2014-09-18 21:02:56 +0100

    add less red color changing funtionality

--- ducks.js
+++ ducks.js
@@ -1,4 +1,7 @@
 var IMAGE_ROTATION = 0;
+var RED_INDEX = 0; 
+
+
 
 $( document ).ready(function() {
     alert( "ready!" );
@@ -15,12 +18,33 @@ $( document ).ready(function() {
 		IMAGE_ROTATION = IMAGE_ROTATION === 0 ? 5 : 0;
 	});
 
-
 	var angle = 0;
 	setInterval(function(){
       	angle+=IMAGE_ROTATION;
      	$("img").rotate(angle);
 	},50);
+
+
+
+	$('#less-red').on("click", function(){
+		var backgroundColor = $('body').css("background-color"); 
+		var rgbValues = backgroundColor.match(/\d+/g);
+		rgbValues[RED_INDEX] -= 10; 
+		var newBackgroundColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + rgbValues[2] + ")" 
+		$('body').css("background-color",newBackgroundColor);
+	});
+
+	$('#more-red').on("click", function(){
+		var backgroundColor = $('body').css("background-color"); 
+		var rgbValues = backgroundColor.match(/\d+/g);
+		console.log(rgbValues)
+		rgbValues[RED_INDEX] += 10; 
+		var newBackgroundColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + rgbValues[2] + ")" 
+		$('body').css("background-color",newBackgroundColor);
+	});
+
+
+
 
 });
 
