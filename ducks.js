75a826aea7942d2d2bf51cc9480514eeba45d9f2
benjamintillett <benjamin.tillett@gmail.com>
2014-09-18 22:01:54 +0100

    add color changes for all rgb colors and refactor js to use change color method

--- ducks.js
+++ ducks.js
@@ -1,6 +1,7 @@
 var IMAGE_ROTATION = 0;
 var RED_INDEX = 0; 
-
+var GREEN_INDEX = 1;
+var BLUE_INDEX = 2;
 
 
 $( document ).ready(function() {
@@ -15,36 +16,48 @@ $( document ).ready(function() {
 	});
 
 	$('#spin').on("click", function(){
-		IMAGE_ROTATION = IMAGE_ROTATION === 0 ? 5 : 0;
+		IMAGE_ROTATION = IMAGE_ROTATION === 0 ? 3 : 0;
 	});
 
 	var angle = 0;
 	setInterval(function(){
       	angle+=IMAGE_ROTATION;
      	$("img").rotate(angle);
-	},50);
+	},1);
 
 
+	$('#less-blue').on("click", function(){
+		changeColor(BLUE_INDEX,-10);
+	});
 
 	$('#less-red').on("click", function(){
-		var backgroundColor = $('body').css("background-color"); 
-		var rgbValues = backgroundColor.match(/\d+/g);
-		rgbValues[RED_INDEX] -= 10; 
-		var newBackgroundColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + rgbValues[2] + ")" 
-		$('body').css("background-color",newBackgroundColor);
+		changeColor(RED_INDEX,-10);
+	});
+
+	$('#less-green').on("click", function(){
+		changeColor(GREEN_INDEX,-10);
 	});
 
 	$('#more-red').on("click", function(){
+		changeColor(RED_INDEX,10);
+	});
+	
+	$('#more-green').on("click", function(){
+		changeColor(GREEN_INDEX,10);
+	});
+	
+	$('#more-blue').on("click", function(){
+		changeColor(BLUE_INDEX,10);
+	});
+});
+
+
+
+function changeColor(color_index,ammount) {
 		var backgroundColor = $('body').css("background-color"); 
 		var rgbValues = backgroundColor.match(/\d+/g);
-		console.log(rgbValues)
-		rgbValues[RED_INDEX] += 10; 
+		rgbValues[color_index] =  parseFloat(rgbValues[color_index]) + ammount; 
 		var newBackgroundColor = "rgb(" + rgbValues[0] + ", " + rgbValues[1] + ", " + rgbValues[2] + ")" 
 		$('body').css("background-color",newBackgroundColor);
-	});
-
-
-
-
-});
+}
 
